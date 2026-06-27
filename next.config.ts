b9d89e5d3fdf7bf161cdd5e0eda17fc1a9d4cbf8
billiegds <billie.delossantos30@gmail.com>
2026-06-27 20:18:00 +0800

    chore: sync latest changes

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*",
+        destination: "https://muscle-api.cloudnext.cloud/api/:path*",
       },
     ];
   },
